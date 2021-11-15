341e5d052702a7dde6582075d4b0b18fe3cf62b6
tyymtic <92346384+tyymtic@users.noreply.github.com>
2021-11-15 23:53:33 +0900

    5-q1

--- section5-q1/sketch.js
+++ section5-q1/sketch.js
@@ -5,7 +5,20 @@ function setup(){
   createCanvas(400, 400);
   background(255);
   balloon("I love keyakizaka46");
+  regularPolygon(3, 80, 25, 10);
 }
+
+function regularPolygon(n, cx, cy, r){
+  beginShape();
+  for(var i = 0; i < n; i++){
+    let theta = TWO_PI * i * 1 / n;
+    let x = cx + cos(theta) * r;
+    let y = cy + sin(theta - 1 / 6 * PI) * r;
+    vertex(x,y);
+  }
+  endShape(CLOSE);
+}
+
 
 function balloon(t){
   let w = textWidth(t);
