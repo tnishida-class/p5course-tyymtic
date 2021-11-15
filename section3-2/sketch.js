8a39abc5e8122fe2fe4dbce145b03d09d0788cd4
tyymtic <92346384+tyymtic@users.noreply.github.com>
2021-11-15 23:24:27 +0900

    3-2

--- section3-2/sketch.js
+++ section3-2/sketch.js
@@ -1,21 +1,24 @@
 // テキスト「アニメーションの基本」
-let x, y, vx, vy;
+let x, y, vx, vy ,vz;
 const g = 1; // 重力加速度
 const vyMax = 30;
 
 function setup(){
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, windowsized);
   x = width / 2;
   y = height / 2;
+  z = size / 2;
   vx = 8;
   vy = 8;
+  vz = 8;
 }
 
 function draw(){
   background(160, 192, 255);
-  ellipse(x, y, 20, 20);
+  ellipse(x, y, z);
   x += vx;
   y += vy;
+  z += vz;
 
   // 重力（コメント機能でオンオフ切り替えて実行してみましょう）
   vy = constrain(vy + g, -vyMax, vyMax);
@@ -29,10 +32,12 @@ function draw(){
 　// 端の処理パターン (2) 跳ね返る
   if(x < 0 || x > width){ vx = -1 * vx; }
   if(y > height){ vy = -1 * vy; }
+  if(z > 20){ vz = -1 * vz; }
   x = constrain(x, 0, width);
   y = constrain(y, 0, height);
+  z = constrain(z, 0, size);
 }
 
 function windowResized(){
-  resizeCanvas(windowWidth, windowHeight);
+  resizeCanvas(windowWidth, windowHeight, windowsized);
 }
