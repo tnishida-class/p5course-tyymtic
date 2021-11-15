0699bf423e9734ed01f34a562a115a8b0cf3ffba
tyymtic <92346384+tyymtic@users.noreply.github.com>
2021-11-15 23:29:59 +0900

    Update sketch.js

--- section5-2/sketch.js
+++ section5-2/sketch.js
@@ -4,30 +4,13 @@ function setup(){
   createCanvas(300, 100);
   background(200);
   fill(0);
-  crossmark(10, 10, 90, 90);
-  ngmark(150, 50, 80);
-  star(250, 50, 40);
+  regularPolygon(7, 50, 40, 40);
 }
 
-function crossmark(x1, y1, x2, y2){
-  line(x1, y1, x2, y2);
-  line(x2, y1, x1, y2);
-}
-
-function ngmark(cx, cy, r){
-  push();
-  noFill();
-  strokeWeight(r * 0.1);
-  let d = sqrt(r * r / 8);
-  ellipse(cx, cy, r);
-  line(cx - d, cy - d, cx + d, cy + d);
-  pop();
-}
-
-function star(cx, cy, r){
+function regularPolygon(n, cx, cy, r){
   beginShape();
-  for(var i = 0; i < 5; i++){
-    let theta = TWO_PI * i * 2 / 5 - HALF_PI;
+  for(var i = 0; i < n; i++){
+    let theta = TWO_PI * i * 1 / n;
     let x = cx + cos(theta) * r;
     let y = cy + sin(theta) * r;
     vertex(x,y);
